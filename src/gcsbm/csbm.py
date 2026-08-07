06c05f1f7f15a3bd7eef6bcf1ef7477dcf3df1cf
tiagodsilva <tdsh97@gmail.com>
2026-08-07 13:59:38 +0400

    update

---
+++ src/gcsbm/csbm.py
@@ -0,0 +1,50 @@
+import flax.struct as struct
+import jax
+import jax.numpy as jnp
+import jax.scipy as jsp
+
+
+@struct.dataclass
+class CSBMParam:
+    label_dist: jax.Array
+    conn_dist: jax.Array
+    mu_dist: jax.Array
+
+    # We assume features are drawn from a label-conditioned isotropic
+    # Gaussian with average mu_dist[label] and variance sigma
+    sigma: float
+
+
+def csbm_log_likelihood(
+    adj: jax.Array,
+    features: jax.Array,
+    labels: jax.Array,
+    theta: CSBMParam,
+):
+    num_nodes = len(adj)
+    # Compute the log-likelihood of the adjacency matrix
+    label_i, label_j = jnp.meshgrid(labels, labels)
+    conn_matrix = theta.conn_dist[label_i, label_j]
+
+    # Select only the upper triangular matrix
+    i, j = jnp.triu_indices(num_nodes, k=1)
+    conn_upper = conn_matrix[j, j]
+    adj_upper = adj[i, j]
+
+    adj_log_prob = jnp.log(
+        adj_upper * conn_upper + (1 - adj_upper) * (1 - conn_upper)
+    ).sum()
+
+    # Compute the log-likelihood of the labels
+    labels_log_prob = jnp.log(theta.label_dist.at[labels]).sum()
+
+    # Compute the log-likelihood of the features
+    features_log_prob = jsp.stats.multivariate_normal.logpdf(
+        features,
+        mean=theta.mu_dist[labels],
+        cov=theta.sigma * jnp.eye(num_nodes),
+    )
+    features_log_prob = jnp.sum(features_log_prob)
+
+    # Compute the CSBM's log-likelihood
+    return adj_log_prob + labels_log_prob + features_log_prob
--- src/gcsbm/sampler.py
+++ src/gcsbm/sampler.py
@@ -1,2 +1,57 @@
-import jax 
+from functools import partial
+
+import jax
 import jax.numpy as jnp
+
+
+def label_score(Y_U: jax.Array, Y_L: jax.Array, A: jax.Array, X: jax.Array):
+    return
+
+
+def sample_from_posterior(key: jax.Array, theta: jax.Array):
+    return
+
+
+def step(
+    carry: tuple[jax.Array, jax.Array, jax.Array],
+    _,
+    A: jax.Array,
+    X: jax.Array,
+    Y_L: jax.Array,
+    K: int,
+):
+    key, Y_U, theta = carry
+
+    nk, kl, kt = jax.random.split(key, 3)
+
+    # We first update Y_U
+    scores = label_score(Y_U, Y_L, A, X)
+    logits = jax.nn.log_softmax(scores, axis=0)
+    labels = jax.random.categorical(kl, logits)
+
+    # We then update theta
+    ntheta = sample_from_posterior(kt, theta)
+
+    # Return carry and scanned output
+    return (nk, labels, ntheta), (labels, ntheta)
+
+
+def sample(
+    labels: jax.Array,
+    theta: jax.Array,
+    A: jax.Array,
+    X: jax.Array,
+    Y: jax.Array,
+    K: int,  # Number of labels
+    steps: int,
+    seed: int = 42,
+):
+    key = jax.random.key(seed)
+
+    _, (labels, thetas) = jax.lax.scan(
+        f=partial(step, A=A, X=X, Y_L=Y, K=K),
+        init=(key, labels, theta),
+        length=steps,
+    )
+
+    return Y_Us, thetas
